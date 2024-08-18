967d3540d25b7918fe2aa7220f538331bd0b2373
coleYab <yabume123@gamil.com>
2024-08-18 22:34:28 +0300

    Feat: Added default parameter for the constructor

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -6,7 +6,7 @@ HTMLNode class to represent in the entire assignment
 from functools import reduce
 
 class HTMLNode:
-    def __init__(self, tag, value, children, props):
+    def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
         self.children = children
@@ -18,14 +18,14 @@ class HTMLNode:
     def props_to_html(self):
         conv_props = lambda x: f'{x[0]}="{x[1]}"'
         if isinstance(self.props, dict):
-            return reduce(
+            return " " + reduce(
                 lambda x, y: f"{x} {conv_props(y)}",
                 self.props.items(), ""
                 ).strip()
 
         return ''
 
-    def __re__(self):
+    def __repr__(self):
         return f"HTMLNode => tag: {self.tag}, value: {self.value}, children: {self.children}, props: {self.props}"
 
     def __eq__(self, o):
